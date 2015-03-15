001bd63f2ca229d85dd4858890aae8f3b6a66367
globalavocado <globalavocado@users.noreply.github.com>
2015-03-15 21:09:22 +0000

    sorted out rspec and comments, all tests passing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,33 +5,38 @@ describe Airport do
 
 	let(:airport){Airport.new}
 
-it 'has a default capacity' do
-	expect(airport.capacity).to eq 10
-end
+	context 'airport capacity' do
 
-it 'can be initialized with a capacity' do
-	airport = Airport.new(:capacity => 20)
-	expect(airport.capacity).to eq 20
-end
+		it 'has a default capacity' do
+			expect(airport.capacity).to eq 10
+		end
 
-it 'has no planes in the hangar when initialized' do
-	expect(airport).to be_empty
-end
+		it 'can be initialized with a capacity' do
+			airport = Airport.new(:capacity => 20)
+			expect(airport.capacity).to eq 20
+		end
 
-it 'can permit planes to land' do
-	plane = double :plane
+		it 'has no planes in the hangar when initialized' do
+			expect(airport).to be_empty
+		end
 
-	expect(plane).to receive(:land!)
+	end
 
-	airport.park(plane)
-end
+	context 'airport operations' do
 
-it 'has one plane in the hangar after one plane has landed' do
-	plane = double :plane
-	allow(plane).to receive(:land!)
-	airport.park(plane)
+		it 'can permit planes to land' do
+			plane = double :plane
+			expect(plane).to receive(:land!)
+			airport.park(plane)
+		end
 
-	expect(airport.hangar).to eq [plane]
-end
+		it 'has one plane in the hangar after one plane has landed' do
+			plane = double :plane
+			allow(plane).to receive(:land!)
+			airport.park(plane)
+			expect(airport.hangar).to eq [plane]
+		end
 
+	end
+	
 end
